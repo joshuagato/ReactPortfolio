d70ac3e1cba172f25482b9f6743cc32e0d49491b
Joshua Gato <joshuagatogato37@gmail.com>
2019-12-24 07:40:53 +0000

    Updates

---
+++ precache-manifest.fe9ae20840f79312a2f9aade5b1d13ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae8850959f08232e420ddc973ce4121",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "810f33b171763a515ab7",
+    "url": "/ReactPortfolio/static/css/2.e0c85d36.chunk.css"
+  },
+  {
+    "revision": "f6f0782d3fadd6dfb43e",
+    "url": "/ReactPortfolio/static/css/main.9230b04f.chunk.css"
+  },
+  {
+    "revision": "810f33b171763a515ab7",
+    "url": "/ReactPortfolio/static/js/2.1648dd18.chunk.js"
+  },
+  {
+    "revision": "f6f0782d3fadd6dfb43e",
+    "url": "/ReactPortfolio/static/js/main.aab1c49c.chunk.js"
+  },
+  {
+    "revision": "c782f8db168aedc646ab",
+    "url": "/ReactPortfolio/static/js/runtime-main.d64cee6a.js"
+  },
+  {
+    "revision": "1a26c990eb18bc85f1fe12a3416d57cf",
+    "url": "/ReactPortfolio/static/media/Cookie-Regular.1a26c990.ttf"
+  },
+  {
+    "revision": "d00ad1a21a03928faa79605d211838b5",
+    "url": "/ReactPortfolio/static/media/about1.d00ad1a2.jpg"
+  },
+  {
+    "revision": "e86a1af6772af1ac4f3e59b0eb502a38",
+    "url": "/ReactPortfolio/static/media/about2.e86a1af6.jpg"
+  },
+  {
+    "revision": "00051dbb5b9c7a7f4dfdf04f1b633445",
+    "url": "/ReactPortfolio/static/media/slide1.00051dbb.jpg"
+  },
+  {
+    "revision": "11c0e84e0b0ec7aa2868d0332720e197",
+    "url": "/ReactPortfolio/static/media/slide2.11c0e84e.jpg"
+  }
+]);
